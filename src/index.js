50bad3064893a765b408739314ae4f3dc7850109
Stéphane Beaudet <stephaneBlitz@gmail.com>
2019-02-25 15:47:02 -0500

    more testing

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 var express = require("express");
 var cors = require("cors");
 var app = express();
-var http = require("http");
 
 var connectCtrl = require("./connectController");
 var listCtrl = require("./listController");
